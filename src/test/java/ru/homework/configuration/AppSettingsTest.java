7070c188cf2371adcee7060a6b8de40a60436923
anna2serg <anna2serg@gmail.com>
2018-07-14 12:50:04 +0300

    tests init

--- src/test/java/ru/homework/configuration/AppSettingsTest.java
+++ src/test/java/ru/homework/configuration/AppSettingsTest.java
@@ -1,31 +1,28 @@
-package ru.homework;
+package ru.homework.configuration;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import ru.homework.configuration.AppSettings;
-
 import org.springframework.shell.jline.InteractiveShellApplicationRunner;
 import org.springframework.shell.jline.ScriptShellApplicationRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(properties = {
 	InteractiveShellApplicationRunner.SPRING_SHELL_INTERACTIVE_ENABLED + "=false",
 	ScriptShellApplicationRunner.SPRING_SHELL_SCRIPT_ENABLED + "=false"
 })
-public class AppTest {
+public class AppSettingsTest {
 
 	@Autowired
 	private AppSettings appSettings; 
 	
 	@Test
 	public void contextLoads() {
-		assertEquals(appSettings.getLocale(), "en");
+		assertTrue(!appSettings.getLocale().equals(""));
 	}
 
 }
